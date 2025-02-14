4a80de0c3916442281b8450788b31a866273d02c
Kushal Arora <kushalarora92@gmail.com>
2025-02-13 23:23:39 -0800

    Deployment + reverting to openai 0.28

--- README.md
+++ README.md
@@ -19,3 +19,53 @@ pip install -r requirements.txt
 ```
 python app.py
 ```
+
+# Preparing for deployment
+- requirements.txt: import aws-wsgi 
+- app.py: import awsgi and add handler function
+
+# Steps to deploy the application
+
+1. Create a new directory for the deployment
+```
+mkdir -p deployment/package
+```
+
+2. [OPTIONAL] Append `deployment` directory to .gitignore
+
+3. [OPTIONAL] Check the version of the installed packages & update requirements.txt
+```
+pip freeze | grep <packages>
+```
+
+4. Install dependencies to a package directory
+```
+pip install -r requirements.txt --target deployment/package
+```
+
+5. Copy application files
+```
+cp app.py deployment/package/
+cp -r templates deployment/package/
+```
+
+6. Create deployment ZIP
+```
+cd deployment/package
+zip -r ../deployment.zip .
+```
+
+7. Create AWS Lambda Function
+
+8. Create a new API Gateway
+
+9. Create a new ACM Certificate
+
+10. Create a custom domain name in API Gateway
+
+11. Configure API mappings
+
+11. Create Route 53 record  with the new domain name
+
+12. Test the API from the new domain name
+
--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 # A flask based application that accepts a prompt from the user and generates two AI images on the page. Show a loader and reset when a new submission is provided.
 
 from flask import Flask, request, jsonify, render_template
-from openai import OpenAI
 import os
+import openai
 from dotenv import load_dotenv
+import awsgi
 
 load_dotenv()
 
 app = Flask(__name__)
 
 # Configure OpenAI API key
-# openai.api_key = os.getenv('OPENAI_API_KEY')
-
-client = OpenAI()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/')
 def main():
@@ -28,13 +27,17 @@ def generate():
         return jsonify({'error': 'No prompt provided'}), 400
     
     try:
-        response = client.images.generate(prompt=prompt, n=1, size="256x256")
-        print(response)
+        response = openai.Image.create(prompt=prompt, n=2, size="256x256")
         # Extract just the URLs from the response
-        image_urls = [img.url for img in response.data]
+        image_urls = [img['url'] for img in response['data']]
         return jsonify({'images': image_urls})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Run the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+# For AWS Lambda
+def handler(event, context):
+    return awsgi.response(app, event, context)
